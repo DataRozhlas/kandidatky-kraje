5b50d9c7179cc13da29656b54165795fad7c7158
Petr Kočí <petr.koci@samizdat.cz>
2024-08-08 14:41:18 +0200

    get raw data

--- srv/01-download.tsx
+++ srv/01-download.tsx
@@ -1,24 +1,30 @@
 import JSZip from 'jszip';
 
+
 const urls = [
-    "https://volby.cz/opendata/ep2004/EP2004_reg_20230223_csv.zip",
-    "https://volby.cz/opendata/ep2004/EP2004_cisel_20230223_csv.zip",
-    "https://volby.cz/opendata/ep2009/EP2009_reg_20230223_csv.zip",
-    "https://volby.cz/opendata/ep2009/EP2009_cisel_20230223_csv.zip",
-    "https://volby.cz/opendata/ep2014/EP2014_reg_20230223_csv.zip",
-    "https://volby.cz/opendata/ep2014/EP2014_cisel_20230223_csv.zip",
-    "https://volby.cz/opendata/ep2019/EP2019reg20190526_csv.zip",
-    "https://volby.cz/opendata/ep2019/EP2019ciselniky20190513_csv.zip",
-    "https://volby.cz/opendata/ep2024/EP2024reg20240506_csv.zip",
-    "https://volby.cz/opendata/ep2024/EP2024ciselniky20240506_csv.zip"
-]
+    "https://volby.cz/opendata/kz2000/KZ2000_reg_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2000/KZ2000_cisel_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2004/KZ2004_reg_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2004/KZ2004_cisel_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2008/KZ2008_reg_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2008/KZ2008_cisel_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2012/KZ2012_reg_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2012/KZ2012_cisel_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2016/KZ2016_reg_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2016/KZ2016_cisel_20230223_csv.zip",
+    "https://volby.cz/opendata/kz2020/KZ2020reg20201004a_csv.zip",
+    "https://volby.cz/opendata/kz2020/KZ2020ciselniky20200918_csv.zip",
+    "https://volby.cz/opendata/kz2024/KZ2024reg20240807_csv.zip",
+    "https://volby.cz/opendata/kz2024/KZ2024ciselniky20240807_csv.zip"]
+
+
 
 // downloas each zip file and extract it
 for (const url of urls) {
     const response = await fetch(url);
     const zip = await JSZip.loadAsync(await response.arrayBuffer());
     const files = Object.keys(zip.files);
-    const matchResult = url.match(/ep(\d{4})/);
+    const matchResult = url.match(/kz(\d{4})/);
     const year = matchResult ? matchResult[1] : "unknown";
     for (const file of files) {
         const fileEntry = zip.file(file);
